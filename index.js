baab17e27e04312a0cdb71b7825efacd8249f5f1
AshAnand34 <aashishanand2019@gmail.com>
2024-07-03 16:36:53 -0700

    Created bar chart that displays the number of school shootings by location type, grouped by school level.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var scenes = ["scene-home", "scene-line-chart", "scene-map", "scene-random", "scene-thank-you"];
 
-var sceneIndex = 0;
+var sceneIndex = 3;
 
 var sliderYear = 1970;
+
+var schoolLevel = "Elementary";
 
 var us, schoolShootingData, stateMapping;
 
@@ -60,10 +62,8 @@ function getCurrentScene(prevIndex) {
             .append("p")
             .text(`The number of school shootings fluctuated between 10 and 63 from 1970 to 2010.
                 However, since then, it has been rapidly increasing with the highest being 251 in the year 2021.`);
-            d3.select(".mainContent")
-            .attr("id", scenes[sceneIndex])
-            .append("svg")
-            .attr("width", 800).attr("height", 600)
+            d3.select("#" + scenes[sceneIndex]).append("svg")
+            .attr("width", 800).attr("height", 400)
             createLineChart();
             break;
         case 2:
@@ -77,12 +77,23 @@ function getCurrentScene(prevIndex) {
                 for each state changed over the years.`);
             d3.select("#" + scenes[sceneIndex])
             .append("svg")
-            .attr("width", 960).attr("height", 600)
+            .attr("width", 760).attr("height", 350);
             createMapChart();
             break;
         case 3:
+            var schoolLevels = [...new Set(schoolShootingData.map(d => d.School_Level))].filter(s => s !== "" && s !== "null" && s !== "Unknown");
             d3.select(".mainContent").attr("id", scenes[sceneIndex])
-            .append("p").text("Random String");
+            .append("p").text("Random String ");
+            d3.select("p").append("select")
+            .attr("id", "schoolLevels").attr("name", "schoolLevels")
+            .selectAll("option").data(schoolLevels)
+            .enter().append("option")
+            .attr("value", function (d) { return d; })
+            .text(function(d) { return d; });
+            d3.select("#" + scenes[sceneIndex])
+            .append("svg")
+            .attr("width", 760).attr("height", 400);
+            createBarChart();
             break;
         case 4:
             d3.select("#right-arrow").attr("disabled", true);
@@ -95,14 +106,16 @@ function getCurrentScene(prevIndex) {
     }
 }
 
-async function createMapChart() {
-    var states = topojson.feature(us, us.objects.states).features;
+function createMapChart() {
+    var states = topojson.feature(us, us.objects.states);
     var borders = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
     var dataMap = d3.rollup(schoolShootingData, (v) => v.length, (d) => d.Date.getFullYear(), (d) => d.State);
     var colorScheme = d3.scaleLinear().domain([0, d3.greatest(dataMap.get(sliderYear), d => d[1])[1]]).range(["white", "maroon"]);
-    var path = d3.geoPath();
+    var projection = d3.geoIdentity().fitSize([760-50,350-50], states)
+    var path = d3.geoPath().projection(projection);
     var abbrevs = d3.sort(stateMapping, (d) => d.State).map(d => d.Abbreviation);
-    var ansiDict = states.map(d => parseInt(d.id)).sort(function(a, b) { return a - b;})
+    var ansiDict = states.features.map(d => parseInt(d.id))
+        .sort(function(a, b) { return a - b;})
         .map(function (d, i) { return [d, abbrevs[i]]; });
 
     var tooltip = d3.select("#" + scenes[sceneIndex])
@@ -112,9 +125,9 @@ async function createMapChart() {
         .style("border", "solid")
         .style("border-width", "1px")
         .style("border-radius", "5px");
-
+    
     d3.select("svg").selectAll("path")
-    .data(states)
+    .data(states.features)
     .enter().append("path")
     .attr("class", "states")
     .attr("fill", function(d) {
@@ -126,9 +139,12 @@ async function createMapChart() {
         tooltip.style("opacity", 1);
     })
     .on("mousemove", function(event, d) {
+        console.log(d);
         var stateANSI = ansiDict.filter(s => s[0] === parseInt(d.id))[0];
         tooltip
-        .html(stateANSI[1] + " had " + (dataMap.get(sliderYear).get(stateANSI[1]) || 0) + " school shootings.");
+        .html(stateANSI[1] + " had " + (dataMap.get(sliderYear).get(stateANSI[1]) || 0) + " school shootings.")
+        .style("top", `${d.y + tooltip.clientHeight/2}px`)
+        .style("left", `${d.x + 35}px`);
     }).on("mouseleave", function(event, d) {
         tooltip.style("opacity", 0);
     });
@@ -166,11 +182,11 @@ async function createMapChart() {
     })
 }
 
-async function createLineChart() {
+function createLineChart() {
     var lineData = d3.flatRollup(schoolShootingData, (v) => v.length, (d) => d.Date.getFullYear());
     var dateExtent = d3.extent(lineData, function(d) { return d[0]; });
     var xScale = d3.scaleLinear().domain(dateExtent).range([0, 700]);
-    var yScale = d3.scaleLinear().domain([0, d3.max(lineData, function(d) { return d[1]; })]).range([500, 0]);
+    var yScale = d3.scaleLinear().domain([0, d3.max(lineData, function(d) { return d[1]; })]).range([300, 0]);
 
     var annotationData = lineData.filter(function(d) { return d[0] === 2021 })[0];
 
@@ -199,8 +215,9 @@ async function createLineChart() {
     
     d3.select("svg")
     .append("text")
-    .attr("x", "400")
-    .attr("y", "600")
+    .attr("x", 400)
+    .attr("y", 400)
+    .attr("dy", -15)
     .attr("fill", "white")
     .style("text-anchor", "middle")
     .text("Year")
@@ -209,9 +226,9 @@ async function createLineChart() {
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("x", 0)
-    .attr("y", 300)
-    .attr("dx", -300)
-    .attr("dy", -285)
+    .attr("y", 200)
+    .attr("dx", -200)
+    .attr("dy", -185)
     .attr("fill", "white")
     .style("text-anchor", "middle")
     .text("# of School Shootings")
@@ -223,7 +240,7 @@ async function createLineChart() {
 
     d3.select("svg")
     .append("g")
-    .attr("transform", "translate(50,550)")
+    .attr("transform", "translate(50,350)")
     .call(d3.axisBottom(xScale).tickFormat(function (d) { return d.toString().replace(",", ""); }));
 
     d3.select("svg")
@@ -245,4 +262,62 @@ async function createLineChart() {
 
     d3.select(".line-annotation-group").selectAll(".connector").attr("stroke", "white")
     d3.select(".line-annotation-group").selectAll(".connector-end").attr("stroke", "maroon").attr("fill", "maroon")
+}
+
+function createBarChart() {
+    var locationRollup = d3.rollup(schoolShootingData.filter(s => s.Location_Type !== "" 
+        && s.Location_Type !== "null" 
+        && s.Location_Type !== "Unknown" 
+        && s.Location_Type !== "ND"), 
+        v => v.length, d => d.School_Level, d => d.Location_Type);
+    var xScale = d3.scaleBand()
+    .domain([...locationRollup.get(schoolLevel).keys()].sort())
+    .range([0, 660]).padding([0.2]);
+    var yScale = d3.scaleLinear()
+    .domain([0, d3.greatest(locationRollup.get(schoolLevel), d => d[1])[1]])
+    .range([300, 0]);
+
+    d3.select("svg").append("g").attr("class", "barYAxis")
+        .attr("transform", "translate(50,50)")
+        .call(d3.axisLeft(yScale));
+
+    d3.select("svg").append("g").attr("class", "barXAxis")
+        .attr("transform", "translate(50,350)")
+        .call(d3.axisBottom(xScale));
+
+    d3.select("svg").append("g").attr("class", "barChart")
+        .attr("transform", "translate(50,50)")
+        .selectAll("rect")
+        .data(locationRollup.get(schoolLevel), function(d) { return d[0] })
+        .enter().append("rect")
+        .attr("x", function(d, i) { return xScale(d[0]); })
+        .attr("y", function(d, i) { return yScale(d[1]); })
+        .attr("width", xScale.bandwidth)
+        .attr("height", function(d, i) { return 300-yScale(d[1]); });
+
+    d3.select("select")
+    .on("change", function(e) {
+        schoolLevel = e.target.value;
+        xScale.domain([...locationRollup.get(schoolLevel).keys()].sort());
+        yScale.domain([0, d3.greatest(locationRollup.get(schoolLevel), d => d[1])[1]])
+        d3.select(".barYAxis").transition().duration(1000).call(d3.axisLeft(yScale))
+        d3.select(".barXAxis").transition().duration(1000).call(d3.axisBottom(xScale))
+
+        var bars =  d3.select(".barChart").selectAll("rect")
+        .data(locationRollup.get(schoolLevel), function(d) { return d[0] })
+
+        bars.enter().append("rect")
+        .attr("x", function(d) { return xScale(d[0]); })
+        .attr("y", function(d) { return yScale(d[1]); })
+        .attr("width", xScale.bandwidth)
+        .attr("height", function(d) { return 300-yScale(d[1]); });
+
+        d3.select(".barChart").selectAll("rect").transition().duration(1000)
+        .attr("x", function(d) { return xScale(d[0]); })
+        .attr("y", function(d) { return yScale(d[1]); })
+        .attr("width", xScale.bandwidth)
+        .attr("height", function(d) { return 300-yScale(d[1]); })
+        
+        bars.exit().remove()
+    })
 }
